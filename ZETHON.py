f9b841ae5bdc19243feab14d5018da2b2258f5dd
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-07-06 15:00:20 +0100

    .

--- ZETHON.py
+++ ZETHON.py
@@ -23,6 +23,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ZETHON").keyx()
+		__import__("ZETHON").check()
 	else:
-		__import__("ZETHON").keyx()
+		__import__("ZETHON").check()
