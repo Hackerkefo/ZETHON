34e501b1d7208e67dfeb4fac571653beaf8de470
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-07-09 09:33:12 +0100

    .

--- ZETHON.py
+++ ZETHON.py
@@ -1,16 +1,3 @@
-import os
-import platform
-import webbrowser
-os.system('termux-setup-storage')
-os.system('git pull')
-try:os.system('mkdir /sdcard/GOZI-DATA')
-except:pass
-try:os.system('mkdir /sdcard/GOZI-DATA/OK')
-except:pass
-try:os.system('mkdir /sdcard/GOZI-DATA/TAP-A2F')
-except:pass
-try:os.system('touch .prox.txt')
-except:pass
 P = '\x1b[1;97m'
 import os,requests
 xr = requests.get("http://ip-api.com/json/").json()
@@ -23,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ZETHON").Brute()
+		__import__("ZETHON").Read()
 	else:
-		__import__("ZETHON").Brute()
+		__import__("ZETHON").Read()
